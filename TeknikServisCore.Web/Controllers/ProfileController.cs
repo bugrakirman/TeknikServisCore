69ad7be286e721ce978f5fc1e97302c3f95fec2c
Bugra Kırman <bugrakirman@gmail.com>
2019-03-29 13:05:19 +0300

    profile controller

---
+++ TeknikServisCore.Web/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TeknikServisCore.DAL;
+using TeknikServisCore.Models.IdentityModels;
+using TeknikServisCore.Models.ViewModels;
+
+namespace TeknikServisCore.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _dbContext;
+
+      
+        public ProfileController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext dbContext)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditProfile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var model = Mapper.Map<RegisterViewModel>(user);
+
+            return View(model);
+        }
+    }
+}
